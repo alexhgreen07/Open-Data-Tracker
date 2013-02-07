4c39fc91c8c128f25cc5fefe90200e73fbf27039
Alex Green <alex.h.green07@gmail.com>
2013-02-07 17:53:38 -0500

    changed data tab to report tab

---
+++ js/report_tab.js
@@ -0,0 +1,25 @@
+function Report_Tab (data_div_id) {
+
+	//class variables
+	this.div_id = data_div_id;
+
+	//render function (div must already exist)
+	this.Render = function() {
+	
+		var return_html = '';
+		
+		return_html += '<form method="post" style="text-align:center;" id="graph_display_form">';
+		return_html += 'Under construnction...';
+		return_html += '</form>';
+		
+		var div_tab = document.getElementById(this.div_id);
+
+		div_tab.innerHTML = return_html;
+		
+
+	};
+}
+
+
+
+

26f8d1b45d1aaaa604c49d04488d2bc630bf5f55
Alex Green <alex.h.green07@gmail.com>
2013-02-08 12:14:36 -0500

    created graph tab skeleton structure.

--- js/graph_tab.js
+++ js/graph_tab.js
@@ -2,19 +2,39 @@ function Graph_Tab (data_div_id) {
 
 	//class variables
 	this.div_id = data_div_id;
-
+	
 	//render function (div must already exist)
 	this.Render = function() {
 	
+		var tabs_array = new Array();
+		
+		var new_tab;
+		
+		new_tab = new Array();
+		new_tab.push("Summaries");
+		new_tab.push('Under construction...');
+		tabs_array.push(new_tab);
+		
+		new_tab = new Array();
+		new_tab.push("Custom Graph");
+		new_tab.push('Under construction...');
+		tabs_array.push(new_tab);
+		
+		new_tab = new Array();
+		new_tab.push("Time Based");
+		new_tab.push('Under construction...');
+		tabs_array.push(new_tab);
+		
 		var return_html = '';
 		
-		return_html += '<form method="post" style="text-align:center;" id="graph_display_form">';
-		return_html += 'Under construnction...';
-		return_html += '</form>';
+		return_html += '<div id="graphs_accordian"></div>';
 		
 		var div_tab = document.getElementById(this.div_id);
-
 		div_tab.innerHTML = return_html;
+		
+		//render the accordian
+		var task_accordian = new Accordian('graphs_accordian',tabs_array);
+		task_accordian.Render();
 		
 
 	};

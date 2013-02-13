dc1d312193d7f3bc20d519fd5e5a6ef64d127954
Alex Green <alex.h.green07@gmail.com>
2013-02-13 09:43:16 -0500

    added categories tabs.

--- js/home_tab.js
+++ js/home_tab.js
@@ -83,22 +83,183 @@ function Home_Tab (home_div_id) {
 		});
 	};
 	
+	this.Render_View_Category_Tab = function(form_div_id)
+	{
+		
+		this.view_category_form = document.createElement("form");
+		this.view_category_form.setAttribute('method',"post");
+		this.view_category_form.setAttribute('id',"home_view_category_form");
+		
+		var div_tab = document.getElementById(form_div_id);
+		div_tab.innerHTML = 'Under construction...';
+		div_tab.appendChild(this.view_category_form);
+	};
+	
+	this.Render_Add_New_Category_Tab = function(form_div_id)
+	{
+		this.add_new_category_form = document.createElement("form");
+		this.add_new_category_form.setAttribute('method',"post");
+		this.add_new_category_form.setAttribute('id',"home_add_new_category_form");
+		
+		this.add_new_category_form.innerHTML += 'Name:<br />';
+		
+		this.add_new_category_name = document.createElement("input");
+		this.add_new_category_name.setAttribute('type',"text");
+		this.add_new_category_name.setAttribute('id',"add_new_category_name");
+		this.add_new_category_form.appendChild(this.add_new_category_name);
+		
+		this.add_new_category_form.innerHTML += 'Description:<br />';
+		
+		this.add_new_category_description = document.createElement("input");
+		this.add_new_category_description.setAttribute('type',"text");
+		this.add_new_category_description.setAttribute('id',"add_new_category_description");
+		this.add_new_category_form.appendChild(this.add_new_category_description);
+		
+		this.add_new_category_form.innerHTML += 'Parent Category:<br />';
+		
+		this.add_new_category_parent_select = document.createElement("select");
+		this.add_new_category_parent_select.setAttribute('id',"add_new_category_parent_select");
+		this.add_new_category_parent_select.innerHTML = '<option>-</option>';
+		this.add_new_category_form.appendChild(this.add_new_category_parent_select);
+		
+		this.add_new_category_form.innerHTML += '<br /><br />';
+		
+		this.add_new_category_submit_button = document.createElement("input");
+		this.add_new_category_submit_button.setAttribute('id','add_new_category_submit_button');
+		this.add_new_category_submit_button.setAttribute('type','submit');
+		this.add_new_category_submit_button.value = 'Submit';
+		this.add_new_category_form.appendChild(this.add_new_category_submit_button);
+		
+		var div_tab = document.getElementById(form_div_id);
+		div_tab.innerHTML = '';
+		div_tab.appendChild(this.add_new_category_form);
+		
+		$('#' + this.add_new_category_submit_button.id).button();
+		$('#' + this.add_new_category_submit_button.id).click(function( event )
+		{
+			//ensure a normal postback does not occur
+			event.preventDefault();
+			
+			
+		});
+	};
+	
+	this.Render_Edit_Category_Tab = function(form_div_id)
+	{
+		this.edit_category_form = document.createElement("form");
+		this.edit_category_form.setAttribute('method',"post");
+		this.edit_category_form.setAttribute('id',"home_edit_category_form");
+		
+		this.edit_category_form.innerHTML += 'Category:<br />';
+		
+		this.edit_category_parent_select = document.createElement("select");
+		this.edit_category_parent_select.setAttribute('id',"edit_category_parent_select");
+		this.edit_category_parent_select.innerHTML = '<option>-</option>';
+		this.edit_category_form.appendChild(this.edit_category_parent_select);
+		
+		this.edit_category_form.innerHTML += '<br /><br />';
+		
+		this.edit_category_form.innerHTML += 'Name:<br />';
+		
+		this.edit_category_name = document.createElement("input");
+		this.edit_category_name.setAttribute('type',"text");
+		this.edit_category_name.setAttribute('id',"edit_category_name");
+		this.edit_category_form.appendChild(this.edit_category_name);
+		
+		this.edit_category_form.innerHTML += 'Description:<br />';
+		
+		this.edit_category_description = document.createElement("input");
+		this.edit_category_description.setAttribute('type',"text");
+		this.edit_category_description.setAttribute('id',"edit_category_description");
+		this.edit_category_form.appendChild(this.edit_category_description);
+		
+		this.edit_category_form.innerHTML += 'Parent Category:<br />';
+		
+		this.edit_category_parent_select = document.createElement("select");
+		this.edit_category_parent_select.setAttribute('id',"edit_category_parent_select");
+		this.edit_category_parent_select.innerHTML = '<option>-</option>';
+		this.edit_category_form.appendChild(this.edit_category_parent_select);
+		
+		this.edit_category_form.innerHTML += '<br /><br />';
+		
+		this.edit_category_submit_button = document.createElement("input");
+		this.edit_category_submit_button.setAttribute('id','edit_category_submit_button');
+		this.edit_category_submit_button.setAttribute('type','submit');
+		this.edit_category_submit_button.value = 'Submit';
+		this.edit_category_form.appendChild(this.edit_category_submit_button);
+		
+		this.edit_category_form.innerHTML += '<br /><br />';		
+		
+		this.edit_category_delete_button = document.createElement("input");
+		this.edit_category_delete_button.setAttribute('id','edit_category_delete_button');
+		this.edit_category_delete_button.setAttribute('type','submit');
+		this.edit_category_delete_button.value = 'Delete';
+		this.edit_category_form.appendChild(this.edit_category_delete_button);
+		
+		var div_tab = document.getElementById(form_div_id);
+		div_tab.innerHTML = '';
+		div_tab.appendChild(this.edit_category_form);
+		
+		$('#' + this.edit_category_submit_button.id).button();
+		$('#' + this.edit_category_delete_button.id).button();
+		
+		$('#' + this.edit_category_submit_button.id).click(function( event )
+		{
+			//ensure a normal postback does not occur
+			event.preventDefault();
+			
+			
+		});
+		
+		$('#' + this.edit_category_delete_button.id).click(function( event )
+		{
+			//ensure a normal postback does not occur
+			event.preventDefault();
+			
+			
+		});
+	};
+	
 	this.Render_General_Home_Form = function(form_div_id)
 	{
+		var tabs_array = new Array();
+		var new_tab;
+		
+		new_tab = new Array();
+		new_tab.push("View Categories");
+		new_tab.push('<div id="home_category_view_tab"></div>');
+		tabs_array.push(new_tab);
+		
+		new_tab = new Array();
+		new_tab.push("New Category");
+		new_tab.push('<div id="home_category_add_new_tab"></div>');
+		tabs_array.push(new_tab);
+		
+		new_tab = new Array();
+		new_tab.push("Edit Category");
+		new_tab.push('<div id="home_category_edit_tab"></div>');
+		tabs_array.push(new_tab);
+		
 		this.general_form = document.createElement("form");
 		this.general_form.setAttribute('method',"post");
 		this.general_form.setAttribute('id',"home_general_form");
 		
 		var div_tab = document.getElementById(form_div_id);
-		div_tab.innerHTML = 'Under construction...';
+		div_tab.innerHTML = '<div id="home_category_accordian_div"></div>';
 		div_tab.appendChild(this.general_form);
+		
+		var category_accordian = new Accordian('home_category_accordian_div',tabs_array);
+		category_accordian.Render();
+		
+		this.Render_View_Category_Tab('home_category_view_tab');
+		this.Render_Add_New_Category_Tab('home_category_add_new_tab');
+		this.Render_Edit_Category_Tab('home_category_edit_tab');
 	};
 	
 	//render function (div must already exist)
 	this.Render = function() {
 		
 		var tabs_array = new Array();
-		
 		var new_tab;
 		
 		new_tab = new Array();
@@ -120,7 +281,6 @@ function Home_Tab (home_div_id) {
 		div_tab.innerHTML = return_html;
 		
 		var items_accordian = new Accordian('home_accordian',tabs_array);
-		
 		items_accordian.Render();
 		
 		

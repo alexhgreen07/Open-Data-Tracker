6b1fe4de466310003791bf05ff97f090da61f5aa
Alex Green <alex.h.green07@gmail.com>
2013-07-14 14:03:59 -0400

    Added doxygen comments to authorization php file.

--- server/auth.php
+++ server/auth.php
@@ -1,5 +1,13 @@
 <?php
+/** \file auth.php
+ * This file contains functions to authorize users.
+ * */
 
+/** \fn function Authorize_Session()
+ * \brief This function attempts to authorize the session with a member.
+ * If it fails the script exits.
+ * \return None.
+ * */
 function Authorize_Session() {
 	//Start session
 	session_start();
@@ -13,6 +21,10 @@ function Authorize_Session() {
 	}
 }
 
+/** \fn function Is_Session_Authorized()
+ * \brief This function checks whether a session is authorized.
+ * \return Returns a boolean true if the member is authorized, otherwise it returns false.
+ * */
 function Is_Session_Authorized() {
 	$return_value = !(!isset($_SESSION['SESS_MEMBER_ID']) || (trim($_SESSION['SESS_MEMBER_ID']) == ''));
 

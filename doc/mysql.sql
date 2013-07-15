b26deb30e35d03785f13c001083ed13fea1b271f
Alex Green <alex.h.green07@gmail.com>
2013-07-14 21:11:09 -0400

    Updated mysql schema with all tables.

--- doc/mysql.sql
+++ doc/mysql.sql
@@ -1,20 +1,138 @@
-#
-# Table structure for table 'members'
-#
+-- phpMyAdmin SQL Dump
+-- version 3.5.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 14, 2013 at 09:07 PM
+-- Server version: 5.5.31-0ubuntu0.12.10.1
+-- PHP Version: 5.4.6-1ubuntu1.2
 
-CREATE TABLE `members` (
-  `member_id` int(11) unsigned NOT NULL auto_increment,
-  `firstname` varchar(100) default NULL,
-  `lastname` varchar(100) default NULL,
-  `login` varchar(100) NOT NULL default '',
-  `passwd` varchar(32) NOT NULL default '',
-  PRIMARY KEY  (`member_id`)
-) TYPE=MyISAM;
-
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
-#
-# Dumping data for table 'members'
-#
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-INSERT INTO `members` (`member_id`, `firstname`, `lastname`, `login`, `passwd`) VALUES("1", "Jatinder", "Thind", "phpsense", "ba018360fc26e0cc2e929b8e071f052d");
+--
+-- Database: `life_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `parent_category_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `category_path` text NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE IF NOT EXISTS `items` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_created` datetime NOT NULL,
+  `name` text NOT NULL,
+  `unit` text NOT NULL,
+  `description` text NOT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_log`
+--
+
+CREATE TABLE IF NOT EXISTS `item_log` (
+  `item_log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `time` datetime NOT NULL,
+  `value` float NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `note` text NOT NULL,
+  PRIMARY KEY (`item_log_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE IF NOT EXISTS `members` (
+  `member_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(100) DEFAULT NULL,
+  `lastname` varchar(100) DEFAULT NULL,
+  `login` varchar(100) NOT NULL DEFAULT '',
+  `passwd` varchar(32) NOT NULL DEFAULT '',
+  PRIMARY KEY (`member_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `task_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `date_created` datetime NOT NULL,
+  `estimated_time` float NOT NULL,
+  `note` text NOT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`task_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_log`
+--
+
+CREATE TABLE IF NOT EXISTS `task_log` (
+  `task_log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_id` int(11) NOT NULL,
+  `start_time` datetime NOT NULL,
+  `status` text NOT NULL,
+  `hours` float NOT NULL,
+  `note` text NOT NULL,
+  PRIMARY KEY (`task_log_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_targets`
+--
+
+CREATE TABLE IF NOT EXISTS `task_targets` (
+  `task_schedule_id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_id` int(11) NOT NULL,
+  `scheduled` tinyint(1) NOT NULL,
+  `scheduled_time` datetime NOT NULL,
+  `recurring` tinyint(1) NOT NULL,
+  `recurrance_type` text NOT NULL,
+  `recurrance_period` float NOT NULL,
+  `recurrance_end_time` datetime NOT NULL,
+  PRIMARY KEY (`task_schedule_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

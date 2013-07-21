e3f3475cbc5f65258c634b75d7f02d6431ca80c2
Alex Green <alex.h.green07@gmail.com>
2013-07-20 21:30:12 -0400

    Updated build doc for recursive file search.

--- build-doc.py
+++ build-doc.py
@@ -11,24 +11,43 @@ def main(args):
 	parser = argparse.ArgumentParser(description='Build the documentation for the code.')
 	args = parser.parse_args(args)
     	
-    	directory_files = os.listdir("./client")
+    	#directory_files = os.listdir("./client")
+    	
+    	fileList = []
+	fileSize = 0
+	folderCount = 0
+	rootdir = "./client"
+
+	for root, subFolders, files in os.walk(rootdir):
+	
+	    folderCount += len(subFolders)
+	    
+	    for file in files:
+	    
+		f = os.path.join(root,file)
+		fileSize = fileSize + os.path.getsize(f)
+		#print(f)
+		fileList.append(f)
+
+    	
+    	
     	javascript_php_files = []
 	
 	print 'Files found: ',
 	
-	for script_file in directory_files:
+	for script_file in fileList:
 		
 		if '.js.php' in script_file:
 			
 			print ('%s,' % script_file),
 			new_script_file = script_file.replace('.js.php','.js')
-			os.rename('./client/' + script_file, './client/' + new_script_file)
+			os.rename(script_file, new_script_file)
 			
 			javascript_php_files.append(new_script_file)
 	
 	print '\n\n'
 	
-	proc = subprocess.Popen('jsdoc -a -d=doc/out client/*', shell=True)
+	proc = subprocess.Popen('jsdoc -a -r -E=./client/external -E=./doc/ -d=./doc/out .', shell=True)
 	proc.wait()
 	
 	print '\n\n'
@@ -39,7 +58,7 @@ def main(args):
 		
 		print ('%s' % script_file),
 		new_script_file = script_file.replace('.js','.js.php')
-		os.rename('./client/' + script_file, './client/' + new_script_file)
+		os.rename(script_file, new_script_file)
 	
 	print '\n\n'
 	

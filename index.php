127596f47dd0e2b50b2317a37217f02474ba0acb
Alex Green <alex.h.green07@gmail.com>
2013-07-29 00:24:50 -0400

    Added more iphone meta tags for full screen.

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 	require_once ('server/config.php');
 
-	$html_output = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	$html_output = '
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;"> 
 		<html xmlns="http://www.w3.org/1999/xhtml">
 			<head>
 				<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />

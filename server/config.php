b251c2f2dd8b5438f5333f6bf6cb66f4d3ca7a66
Alex Green <alex.h.green07@gmail.com>
2013-08-09 00:05:16 -0400

    Changed config to generic user.

--- server/config.php
+++ server/config.php
@@ -5,6 +5,6 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'life_management');
-define('DB_PASSWORD', 'agreen11');
+define('DB_PASSWORD', 'password');
 define('DB_DATABASE', 'life_management');
 ?>

6628133c16d1470516b1c75e4b50514f6fe45fb1
Alex Green <alex.h.green07@gmail.com>
2013-11-12 18:22:33 -0500

    Added function to convert server date time.

--- client/core/types.js
+++ client/core/types.js
@@ -19,3 +19,20 @@ function Cast_String_To_Type(type, value)
 	
 	return cast_value;
 }
+
+function Cast_Server_Datetime_to_Date(server_datetime_string)
+{
+	var time_array = server_datetime_string.split(/[- :]/);
+	
+	year = time_array[0];
+	month = time_array[1] - 1;
+	day = time_array[2];
+	hours = time_array[3];
+	minutes = time_array[4];
+	seconds = time_array[5];
+	milliseconds = 0;
+	
+	return_value = Date(year, month, day, hours, minutes, seconds, milliseconds);
+	
+	return return_value;
+}

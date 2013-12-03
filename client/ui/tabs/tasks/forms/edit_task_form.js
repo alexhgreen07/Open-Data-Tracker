1fc3cfa6a72b4ccde72fc61d69323cfe3628fec9
Alex Green <alex.h.green07@gmail.com>
2013-12-02 21:00:40 -0500

    Added select helper to edit task form.

--- client/ui/tabs/tasks/forms/edit_task_form.js
+++ client/ui/tabs/tasks/forms/edit_task_form.js
@@ -10,25 +10,11 @@ function Edit_Task_Form(){
 		//ensure the task info array is saved
 		self.task_info_json_array = data.tasks;
 
-		//create a list of options for the select
-		var new_inner_html = '';
-
-		new_inner_html += '<option>-</option>';
-
-		//iterate through all tasks
-		for (var i = 0; i < self.task_info_json_array.length; i++) {
-			//add task option to select
-			new_inner_html += '<option>' + self.task_info_json_array[i].name + '</option>';
-
-			//format task start datetime
-			if (self.task_info_json_array[i].start_time != '') {
-				//change start date string to javascript date object
-				//var t = self.task_info_json_array[i].start_time.split(/[- :]/);
-				//self.task_info_json_array[i].start_time = new Date(t[0], t[1] - 1, t[2], t[3], t[4], t[5]);
-			}
-		}
-
-		document.getElementById(self.task_entry_task_edit_name_select.id).innerHTML = new_inner_html;
+		Refresh_Select_HTML_From_Table(
+			self.task_entry_task_edit_name_select.id,
+			data.tasks,
+			"name",
+			"name");
 		
 	};
 	

3874cebee257b90c1c274f817f400f6f07d44e54
Alex Green <alex.h.green07@gmail.com>
2014-01-20 23:49:29 -0500

    Added file manifest for caching.

--- member-index.php
+++ member-index.php
@@ -22,6 +22,8 @@ if (!Is_Authorized()) {
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
+<html manifest="manifest.php">
+
 <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;"> 
 <meta name="apple-mobile-web-app-capable" content="yes"/>
 

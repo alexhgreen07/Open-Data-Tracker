fdc5a3911fdfaee881c8b7d9071995a2f4ca78a6
Alex Green <alex.h.green07@gmail.com>
2014-01-26 18:51:39 -0500

    Added skeleton for calendar tab.

---
+++ client/ui/tabs/calendar/calendar_tab.js
@@ -0,0 +1,26 @@
+/** This is the home tab class which holds all UI objects for general data.
+ * @constructor Home_Tab
+ */
+function Calendar_Tab() {
+
+	
+
+	/** @method Refresh_Data
+	 * @desc This function retrieves the home data from the server.
+	 * @param {function} data The callback to call after the refresh of data has completed.
+	 * */
+	this.Refresh = function(data) {
+		
+	};
+
+	/** @method Render
+	 * @desc This function will render the tab in the div that it was initialized with.
+	 * */
+	this.Render = function(home_div_id) {
+		
+		
+		//document.getElementById(home_div_id).innerHTML = 'Hello world!';
+		
+	};
+}
+

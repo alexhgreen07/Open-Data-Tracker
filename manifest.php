87e93a12ea3c87aa9592c79437418db4f1d2b060
Alex Green <alex.h.green07@gmail.com>
2014-02-03 00:56:36 -0500

    Updated manifest for proper file caching.

--- manifest.php
+++ manifest.php
@@ -33,7 +33,18 @@ function create_manifest($folder) {
 	}
 }
 
-create_manifest(".");
+$files = array(
+	"client/main.js.php",
+	"client/main.css.php",
+	"ajax-loader.gif",
+	"externals/js.tree/images/resnyanskiy-tree-icons.png");
+
+foreach($files as $file)
+{
+	echo str_replace(' ', '%20',$file) . "\n";
+	
+	$hashes .= md5_file($file);
+}
 
 // Write the $hashes string
 echo "# Hash: " . md5($hashes) . "\n";

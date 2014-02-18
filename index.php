da8107b5169a80e9518bd0c4dcd722a63e47e751
Alex Green <alex.h.green07@gmail.com>
2014-02-17 20:23:40 -0500

    Added touch and website icon.

--- index.php
+++ index.php
@@ -21,6 +21,8 @@
 	
 	$html_output = '
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<link rel="shortcut icon" href="apple-touch-icon.png" />
+	<link href="apple-touch-icon.png" rel="apple-touch-icon" />
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;"> 
 		<html xmlns="http://www.w3.org/1999/xhtml">
 			<head>
--- logout.php
+++ logout.php
@@ -14,6 +14,8 @@ Logout_Authorized_Session();
 session_destroy();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<link rel="shortcut icon" href="apple-touch-icon.png" />
+<link href="apple-touch-icon.png" rel="apple-touch-icon" />
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />

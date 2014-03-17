05b63feb18a5e1e7516754c9297b388c397e37ce
Alex Green <alex.h.green07@gmail.com>
2014-03-17 00:09:49 -0400

    Added skeleton for task data interface.

---
+++ server/data_interface_lib/task_data_interface.js
@@ -0,0 +1,80 @@
+var database = require('./../database.js');
+
+module.exports = {
+	Insert_Task_Entry: function(params, session, callback)
+	{
+		
+	},
+	Update_Task_Entry: function(params, session, callback)
+	{
+		
+	},
+	Delete_Task_Entry: function(params, session, callback)
+	{
+		
+	},
+	Insert_Task: function(params, session, callback)
+	{
+		
+	},
+	Delete_Task: function(params, session, callback)
+	{
+		
+	},
+	Update_Task: function(params, session, callback)
+	{
+		
+	},
+	Insert_Task_Target: function(params, session, callback)
+	{
+		
+	},
+	Update_Task_Target: function(params, session, callback)
+	{
+		
+	},
+	Delete_Task_Target: function(params, session, callback)
+	{
+		
+	},
+	Get_Tasks_Schema: function(params, session, callback)
+	{
+		
+	},
+	Get_Tasks: function(params, session, callback)
+	{
+		
+	},
+	Get_Task_Log_Schema: function(params, session, callback)
+	{
+		
+	},
+	Get_Task_Log: function(params, session, callback)
+	{
+		
+	},
+	Get_Task_Targets_Schema: function(params, session, callback)
+	{
+		
+	},
+	Get_Task_Targets: function(params, session, callback)
+	{
+		
+	},
+	Break_Recuring_Child: function(params, session, callback)
+	{
+		
+	},
+	Insert_Recurring_Children: function(params, session, callback)
+	{
+		
+	},
+	Update_Recurring_Children: function(params, session, callback)
+	{
+		
+	},
+	Delete_Recurring_Children: function(params, session, callback)
+	{
+		
+	},
+};

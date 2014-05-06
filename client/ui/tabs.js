97b242dfa2f8529256fc38929808f216054c468d
Alex Green <alex.h.green07@gmail.com>
2014-05-05 20:18:27 -0400

    Re-factored tabs for class structure consistency.

--- client/ui/tabs.js
+++ client/ui/tabs.js
@@ -1,58 +1,61 @@
 define(['jquery.ui'],function($){
-	return {
-		/** This is a wrapper for an array of panes in a JQuery tab object.
-		 * @constructor Tabs
-		 */
-		Tabs: function (tab_div, tab_array) {
-			
-			var self = this;
-			
-			//class variables
-			self.tab_div = tab_div;
-			self.tabs = tab_array;
-			self.activate_callback = function(){};
+	
+	/** This is a wrapper for an array of panes in a JQuery tab object.
+	 * @constructor Tabs
+	 */
+	function Tabs(tab_div, tab_array) {
+		
+		var self = this;
+		
+		//class variables
+		self.tab_div = tab_div;
+		self.tabs = tab_array;
+		self.activate_callback = function(){};
 
-			//render function (div must already exist)
-			self.Render = function() {
+		//render function (div must already exist)
+		self.Render = function() {
 
-				var new_inner_html = '';
+			var new_inner_html = '';
+			
+			var ul_container = document.createElement("ul");
+			ul_container = self.tab_div.appendChild(ul_container);
+
+			//create all top tab items using ul
+			for (var i = 0; i < self.tabs.length; i++) {
 				
-				var ul_container = document.createElement("ul");
-				ul_container = self.tab_div.appendChild(ul_container);
+				var li_container = document.createElement("li");
+				li_container = ul_container.appendChild(li_container);
+				
+				var a_container = document.createElement("a");
+				a_container.href = '#' + self.tab_div.id + '-tab-' + i + '';
+				a_container.innerHTML = this.tabs[i][0];
+				a_container = li_container.appendChild(a_container);
+				
+			}
 
-				//create all top tab items using ul
-				for (var i = 0; i < self.tabs.length; i++) {
-					
-					var li_container = document.createElement("li");
-					li_container = ul_container.appendChild(li_container);
-					
-					var a_container = document.createElement("a");
-					a_container.href = '#' + self.tab_div.id + '-tab-' + i + '';
-					a_container.innerHTML = this.tabs[i][0];
-					a_container = li_container.appendChild(a_container);
-					
-				}
+			//create all content tab items using div
+			for (var i = 0; i < self.tabs.length; i++) {
+				
+				var div_container = document.createElement("div");
+				div_container.id = self.tab_div.id + '-tab-' + i + '';
+				div_container = self.tab_div.appendChild(div_container);
+				
+				self.tabs[i][1] = div_container.appendChild(self.tabs[i][1]);
+				
+			}
 
-				//create all content tab items using div
-				for (var i = 0; i < self.tabs.length; i++) {
-					
-					var div_container = document.createElement("div");
-					div_container.id = self.tab_div.id + '-tab-' + i + '';
-					div_container = self.tab_div.appendChild(div_container);
-					
-					self.tabs[i][1] = div_container.appendChild(self.tabs[i][1]);
-					
-				}
+			//execute JQuery tabs initialization
+			$(self.tab_div).tabs({
+				activate: function(event, ui) {
+			        self.activate_callback();
+			   },
+			});
 
-				//execute JQuery tabs initialization
-				$(self.tab_div).tabs({
-					activate: function(event, ui) {
-				        self.activate_callback();
-				   },
-				});
-
-			};
-		}
+		};
+	}
+	
+	return {
+		Tabs: Tabs,
 	};
 });
 

63fc2a82b7e028de96de95257a5c059558563ee7
Alex Green <alex.h.green07@gmail.com>
2014-05-12 20:09:14 -0400

    Re-factored data interface class definition.

--- server/data_interface.js
+++ server/data_interface.js
@@ -58,32 +58,216 @@ define([
 		return diff_table;
 	}
 	
-	return {
-		Data_Interface: function(){
+	function Data_Interface(){
+		
+		var self = this;
+		
+		this.Refresh_All_Data = function(params, session, callback){
+		
+			var return_object = {};
 			
-			var self = this;
+			return_object.data = {};
+			return_object.data["Categories"] = [];
+			return_object.data["items"] = [];
+			return_object.data["item_entries"] = [];
+			return_object.data["item_targets"] = [];
+			return_object.data["tasks"] = [];
+			return_object.data["task_entries"] = [];
+			return_object.data["task_targets"] = [];
 			
-			this.Refresh_All_Data = function(params, session, callback){
+			return_object.schema = {};
+			return_object.schema["Categories"] = [];
+			return_object.schema["items"] = [];
+			return_object.schema["item_entries"] = [];
+			return_object.schema["item_targets"] = [];
+			return_object.schema["tasks"] = [];
+			return_object.schema["task_entries"] = [];
+			return_object.schema["task_targets"] = [];
 			
-				var return_object = {};
+			return_object.settings = {};
+			return_object.settings["settings"] = [];
+			return_object.settings["setting_entries"] = [];
+			
+			return_object.reports = [];
+			
+			var counter = 0;
+			
+			var counter_callback = function()
+			{
+				counter -= 1;
+				if(counter == 0)
+				{
+					var params = {};
+					
+					params.data = return_object.data;
+					
+					if(!session.data)
+					{
+						session.data = return_object.data;
+					}
+					
+					home_data_interface.Get_Homepage_Report(params, session, function(object){
+						
+						return_object["home_report"] = object;
+						
+						callback(return_object);
+						
+					});
+					
+				}
+			};
+			
+			var queries = [];
+			
+			queries.push({
+				method: home_data_interface.Get_Categories,
+				callback: function(object){
+					
+					return_object.data["Categories"] = object;
+					counter_callback();
+					
+				}});
+				
+			queries.push({
+				method: item_data_interface.Get_Item_Log,
+				callback: function(object){
+					
+					return_object.data["item_entries"] = object;
+					counter_callback();
+					
+				}});
+				
+			queries.push({
+				method: item_data_interface.Get_Items,
+				callback: function(object){
+					
+					return_object.data["items"] = object;
+					counter_callback();
+					
+				}});
+				
+			queries.push({
+				method: item_data_interface.Get_Item_Targets,
+				callback: function(object){
+					
+					return_object.data["item_targets"] = object;
+					counter_callback();
+					
+				}});
+			
+			queries.push({
+				method: task_data_interface.Get_Tasks,
+				callback: function(object){
+					
+					return_object.data["tasks"] = object;
+					counter_callback();
+					
+				}});
+			
+			queries.push({
+				method: task_data_interface.Get_Task_Log,
+				callback: function(object){
+					
+					return_object.data["task_entries"] = object;
+					counter_callback();
+					
+				}});
+			
+			queries.push({
+				method: task_data_interface.Get_Task_Targets,
+				callback: function(object){
+					
+					return_object.data["task_targets"] = object;
+					counter_callback();
+					
+				}});
+			
+			queries.push({
+				method: home_data_interface.Get_Categories_Schema,
+				callback: function(object){
+					
+					return_object.schema["Categories"] = object;
+					counter_callback();
+					
+				}});
+				
+			queries.push({
+				method: item_data_interface.Get_Item_Log_Schema,
+				callback: function(object){
+					
+					return_object.schema["item_entries"] = object;
+					counter_callback();
+					
+				}});
+				
+			queries.push({
+				method: item_data_interface.Get_Items_Schema,
+				callback: function(object){
+					
+					return_object.schema["items"] = object;
+					counter_callback();
+					
+				}});
+				
+			queries.push({
+				method: item_data_interface.Get_Item_Targets_Schema,
+				callback: function(object){
+					
+					return_object.schema["item_targets"] = object;
+					counter_callback();
+					
+				}});
+			
+			queries.push({
+				method: task_data_interface.Get_Tasks_Schema,
+				callback: function(object){
+					
+					return_object.schema["tasks"] = object;
+					counter_callback();
+					
+				}});
+			
+			queries.push({
+				method: task_data_interface.Get_Task_Log_Schema,
+				callback: function(object){
+					
+					return_object.schema["task_entries"] = object;
+					counter_callback();
+					
+				}});
+			
+			queries.push({
+				method: task_data_interface.Get_Task_Targets_Schema,
+				callback: function(object){
+					
+					return_object.schema["task_targets"] = object;
+					counter_callback();
+					
+				}});
+			
+			counter = queries.length;
+			
+			for(var key in queries)
+			{
+				queries[key].method({}, session,queries[key].callback);
+			}
+			
+		};
+		
+		this.Refresh_From_Session_Diff = function(params, session, callback){
+			
+			var return_object = {};
+			
+			self.Refresh_All_Data(params, session, function(object){
 				
 				return_object.data = {};
-				return_object.data["Categories"] = [];
-				return_object.data["items"] = [];
-				return_object.data["item_entries"] = [];
-				return_object.data["item_targets"] = [];
-				return_object.data["tasks"] = [];
-				return_object.data["task_entries"] = [];
-				return_object.data["task_targets"] = [];
-				
-				return_object.schema = {};
-				return_object.schema["Categories"] = [];
-				return_object.schema["items"] = [];
-				return_object.schema["item_entries"] = [];
-				return_object.schema["item_targets"] = [];
-				return_object.schema["tasks"] = [];
-				return_object.schema["task_entries"] = [];
-				return_object.schema["task_targets"] = [];
+				return_object.data["Categories"] = Diff_Table(session.data["Categories"], object.data["Categories"], 'Category ID');
+				return_object.data["items"] = Diff_Table(session.data["items"], object.data["items"], 'item_id');
+				return_object.data["item_entries"] = Diff_Table(session.data["item_entries"], object.data["item_entries"], 'item_log_id');
+				return_object.data["item_targets"] = Diff_Table(session.data["item_targets"], object.data["item_targets"], 'item_target_id');
+				return_object.data["tasks"] = Diff_Table(session.data["tasks"], object.data["tasks"], 'task_id');
+				return_object.data["task_entries"] = Diff_Table(session.data["task_entries"], object.data["task_entries"], 'task_log_id');
+				return_object.data["task_targets"] = Diff_Table(session.data["task_targets"], object.data["task_targets"], 'task_schedule_id');
 				
 				return_object.settings = {};
 				return_object.settings["settings"] = [];
@@ -91,215 +275,33 @@ define([
 				
 				return_object.reports = [];
 				
-				var counter = 0;
+				session.data = object.data;
 				
-				var counter_callback = function()
-				{
-					counter -= 1;
-					if(counter == 0)
-					{
-						var params = {};
-						
-						params.data = return_object.data;
-						
-						if(!session.data)
-						{
-							session.data = return_object.data;
-						}
-						
-						home_data_interface.Get_Homepage_Report(params, session, function(object){
-							
-							return_object["home_report"] = object;
-							
-							callback(return_object);
-							
-						});
-						
-					}
-				};
+				callback(return_object);
 				
-				var queries = [];
-				
-				queries.push({
-					method: home_data_interface.Get_Categories,
-					callback: function(object){
-						
-						return_object.data["Categories"] = object;
-						counter_callback();
-						
-					}});
-					
-				queries.push({
-					method: item_data_interface.Get_Item_Log,
-					callback: function(object){
-						
-						return_object.data["item_entries"] = object;
-						counter_callback();
-						
-					}});
-					
-				queries.push({
-					method: item_data_interface.Get_Items,
-					callback: function(object){
-						
-						return_object.data["items"] = object;
-						counter_callback();
-						
-					}});
-					
-				queries.push({
-					method: item_data_interface.Get_Item_Targets,
-					callback: function(object){
-						
-						return_object.data["item_targets"] = object;
-						counter_callback();
-						
-					}});
-				
-				queries.push({
-					method: task_data_interface.Get_Tasks,
-					callback: function(object){
-						
-						return_object.data["tasks"] = object;
-						counter_callback();
-						
-					}});
-				
-				queries.push({
-					method: task_data_interface.Get_Task_Log,
-					callback: function(object){
-						
-						return_object.data["task_entries"] = object;
-						counter_callback();
-						
-					}});
-				
-				queries.push({
-					method: task_data_interface.Get_Task_Targets,
-					callback: function(object){
-						
-						return_object.data["task_targets"] = object;
-						counter_callback();
-						
-					}});
-				
-				queries.push({
-					method: home_data_interface.Get_Categories_Schema,
-					callback: function(object){
-						
-						return_object.schema["Categories"] = object;
-						counter_callback();
-						
-					}});
-					
-				queries.push({
-					method: item_data_interface.Get_Item_Log_Schema,
-					callback: function(object){
-						
-						return_object.schema["item_entries"] = object;
-						counter_callback();
-						
-					}});
-					
-				queries.push({
-					method: item_data_interface.Get_Items_Schema,
-					callback: function(object){
-						
-						return_object.schema["items"] = object;
-						counter_callback();
-						
-					}});
-					
-				queries.push({
-					method: item_data_interface.Get_Item_Targets_Schema,
-					callback: function(object){
-						
-						return_object.schema["item_targets"] = object;
-						counter_callback();
-						
-					}});
-				
-				queries.push({
-					method: task_data_interface.Get_Tasks_Schema,
-					callback: function(object){
-						
-						return_object.schema["tasks"] = object;
-						counter_callback();
-						
-					}});
-				
-				queries.push({
-					method: task_data_interface.Get_Task_Log_Schema,
-					callback: function(object){
-						
-						return_object.schema["task_entries"] = object;
-						counter_callback();
-						
-					}});
-				
-				queries.push({
-					method: task_data_interface.Get_Task_Targets_Schema,
-					callback: function(object){
-						
-						return_object.schema["task_targets"] = object;
-						counter_callback();
-						
-					}});
-				
-				counter = queries.length;
-				
-				for(var key in queries)
-				{
-					queries[key].method({}, session,queries[key].callback);
-				}
-				
-			};
+			});
 			
-			this.Refresh_From_Session_Diff = function(params, session, callback){
-				
-				var return_object = {};
-				
-				self.Refresh_All_Data(params, session, function(object){
-					
-					return_object.data = {};
-					return_object.data["Categories"] = Diff_Table(session.data["Categories"], object.data["Categories"], 'Category ID');
-					return_object.data["items"] = Diff_Table(session.data["items"], object.data["items"], 'item_id');
-					return_object.data["item_entries"] = Diff_Table(session.data["item_entries"], object.data["item_entries"], 'item_log_id');
-					return_object.data["item_targets"] = Diff_Table(session.data["item_targets"], object.data["item_targets"], 'item_target_id');
-					return_object.data["tasks"] = Diff_Table(session.data["tasks"], object.data["tasks"], 'task_id');
-					return_object.data["task_entries"] = Diff_Table(session.data["task_entries"], object.data["task_entries"], 'task_log_id');
-					return_object.data["task_targets"] = Diff_Table(session.data["task_targets"], object.data["task_targets"], 'task_schedule_id');
-					
-					return_object.settings = {};
-					return_object.settings["settings"] = [];
-					return_object.settings["setting_entries"] = [];
-					
-					return_object.reports = [];
-					
-					session.data = object.data;
-					
-					callback(return_object);
-					
-				});
-				
-				
-				
-			};
-						
-			this.Get_Session_Data = function(params, session, callback){
-				
-				callback({});
-				
-			};
 			
-			this.Save_Session_Data = function(params, session, callback)
-			{
-				//get the parameters
-				data = params.data;
-				
-				callback({});
-			};
 			
-		},
+		};
+					
+		this.Get_Session_Data = function(params, session, callback){
+			
+			callback({});
+			
+		};
+		
+		this.Save_Session_Data = function(params, session, callback)
+		{
+			//get the parameters
+			data = params.data;
+			
+			callback({});
+		};
+		
+	}
+	
+	return {
+		Data_Interface: Data_Interface,
 	};
 });
